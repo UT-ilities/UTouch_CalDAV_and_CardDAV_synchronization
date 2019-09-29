d965c49d459e7daa7e95fe07c61441537700666b
SaltyCybernaut <SaltyCybernaut@protonmail.ch>
2019-09-28 22:23:03 -0700

    Enable mult-configuration support.

--- README.md
+++ README.md
@@ -6,21 +6,28 @@ This utility is a fancy wrapper around `syncevolution` and `cron` and it creates
 
 Configure server URLs, credentials, and naming preferences.
 
-    vim config.txt
+    cp config-nextcloud-template.txt config-personal.txt
+    vim config-personal.txt
 
-Executing `setup-dav-sync.sh --contacts` or `setup-dav-sync.sh --calendar` or `setup-dav-sync.sh --contacts --calendar` will read your configurations, connect to the specified carddav/caldav server, synchronize data and setup a cron job to keep this device in sync with the server.
+Executing `setup-dav-sync.sh --contacts config-personal.txt` or `setup-dav-sync.sh --calendar config-personal.txt` will read your configurations, connect to the specified carddav/caldav server, synchronize data and setup a cron job to keep this device in sync with the server.
 
-Executing `setup-dav-sync.sh --delete-contacts` or `setup-dav-sync.sh --delete-calendar` or `setup-dav-sync.sh --delete-contacts --delete-calendar` will read your configurations, remove them from `syncevolution` configurations and remove all data from this device only (the data on the server will not be affected).
+Executing `setup-dav-sync.sh --delete-contacts config-personal.txt` or `setup-dav-sync.sh --delete-calendar config-personal.txt` will read your configurations, remove them from `syncevolution` configurations and remove all data from this device only (the data on the server will not be affected).
 
 # Using Multiple Carddav/Caldav Servers.
 
-It is possible to use this utility with multiple servers; the easiest approach is to create a config file for each server (i.e. `config-nextcloud.txt`, `config-google.txt`) and symbolically link one config file at a time to `config.txt`.
+Simple specify multiple configurations files and a single command will process them all.
 
-    ln --symbolic --force --no-target-directory configs-nextcloud.txt configs.txt
+    setup-dav-sync.sh --contacts --calendar config-personal.txt holidays.txt team-awesome-schedule.txt
 
 # Known Bugs
 
-When a contact is delete from Ubuntu Touch using the native Contacts app `syncevolution` will relay this change to the carddav server as a 'modification' of the contact and it will not be deleted from the server and other clients synchronized with the server will still have this _old_ contact.
+1. When a contact is delete from Ubuntu Touch using the native Contacts app `syncevolution` will relay this change to the carddav server as a 'modification' of the contact and it will not be deleted from the server and other clients synchronized with the server will still have this _old_ contact.
+2. Even after a calendar has been removed it will still show up as an available calendar in the Ubuntu Touch native calendar app.
+
+# Recommendations for Nextcloud Users.
+
+I do NOT recommend using this utility to synchronize calendars hosted in Nextcloud. The online accounts feature of Ubuntu Touch does a better job.
+I do recommend using this to synchronize contacts.
 
 # Pull Requests Are Welcomed
 I am only able to test configurations and servers I use, which currently is Nextcloud. I will be happy to incorporate changes which allow a larger audience to benefit from this project.
--- setup-dav-sync.sh
+++ setup-dav-sync.sh
@@ -1,136 +1,307 @@
 #!/usr/bin/env bash
 
-CONFIGS='configs.txt'
-source $CONFIGS
+function generate_padding {
+    # get the first character ':0:1' - aka index zero to one
+    local symbol="${1:0:1}"
+    shift # drop input parameter '1' and shift all parameters to the left
 
-function cron_setup {
-    ACTION="$1" # either 'add' or 'delete'
-    REGEX_SEARCH="$2"
-    
+    local -i offset=$1
+    shift
 
-    CRON_TAB="/var/spool/cron/crontabs/$USER"
+    declare -g padding=''
 
+    # iterate over the remaining input parameters
+    local param
+    for param in "$@"; do
+        # iterate over string length
+        local i
+        for (( i = 0; i < ${#param}; i++ )); do
+            padding="$padding$symbol"
+        done
+    done
 
-function setup_sync {
-    SYNC="export DISPLAY=:0.0 && export DBUS_SESSION_BUS_ADDRESS=\$(ps -u $USER e | grep -Eo 'dbus-daemon.*address=unix:abstract=/tmp/dbus-[A-Za-z0-9]{10}' | tail -c35) && /usr/bin/syncevolution $1"
-    CRON_SYNC="$CRON_FREQUENCY $SYNC"
-    REGEX_SEARCH="^.*$1.*$"
-    CRON_TAB="/var/spool/cron/crontabs/$USER"
+    # include padding for the gaps between words
+    local i
+    for (( i = 0; i < $# - 1; i++ )); do
+        padding="$padding$symbol"
+    done
+
+    local i
+    for (( i = 0; i < $offset; i++ )); do
+        padding="$padding$symbol"
+    done
+}
+
+function cron_handler {
+    local action="$1" # either 'add' or 'delete'
+    local server_config_name="$2"
+    local cron_entry="$3"
+
+    local cron_tab="/var/spool/cron/crontabs/$USER"
+    local regex_search="^.*$server_config_name.*$"
 
     sudo mount / -o remount,rw
-    
-    if [ ! -f $CRON_TAB ]; then
-        sudo touch $CRON_TAB
-        sudo chown $USER:clickpkg $CRON_TAB # non portable operation
-        sudo chmod 600 $CRON_TAB
-    fi
 
-    GREP_RESULT=$(sudo grep --only-matching "$REGEX_SEARCH" $CRON_TAB)
+    case "$action" in
+        'add')
+            if sudo [ -f "$cron_tab" ]; then
+                local grep_result=\
+                    $(sudo grep --only-matching "$regex_search" "$cron_tab")
 
-    # if a matching cron entry exists replace it
-    if [ ! -z "$GREP_RESULT" ]; then
-        sudo sed --in-place --expression="s|$REGEX_SEARCH|$CRON_SYNC|" $CRON_TAB
-    else
-        echo "$CRON_SYNC" | sudo tee $CRON_TAB &>/dev/null
-    fi
+                # if a matching cron entry exists replace it
+                if [ ! -z "$grep_result" ]; then
+                    echo "cron - updating entry for '$server_config_name'"
+                    sudo sed --in-place --expression="s|$regex_search|$cron_entry|" "$cron_tab"
+                else
+                    echo "cron - creating new entry for '$server_config_name'"
+                    echo "$cron_entry" | sudo tee  --append "$cron_tab" &>/dev/null
+                fi
+            else
+                echo "User's cron tab not found, creating it."
+                sudo touch "$cron_tab"
+                sudo chown $USER:clickpkg "$cron_tab" # non portable operation
+                sudo chmod 600 "$cron_tab"
 
-    sudo service cron restart
+                echo "$cron_entry" | sudo tee  --append "$cron_tab" &>/dev/null
+            fi
+        ;;
+        'delete')
+            if sudo [ -f "$cron_tab" ]; then
+                local grep_result=$(sudo grep --only-matching "$regex_search" "$cron_tab")
+
+                # if a matching cron entry exists delete it
+                if [ ! -z "$grep_result" ]; then
+                    echo "cron - deleting entry for '$server_config_name'"
+                    sudo sed --in-place --expression="/$regex_search/d" "$cron_tab"
+                else
+                    echo "cron - no entry found for '$server_config_name'"
+                fi
+            else
+                echo "User's cron tab not found, no action taken on cron tab."
+            fi
+        ;;
+        *)
+            sudo mount / -o remount,ro
+            echo 'Internal error!' >&2
+            echo "in the '${FUNCNAME[0]}' function" >&2
+            exit 1
+        ;;
+    esac
+
+    sudo service cron restart &>/dev/null
+
+    sudo mount / -o remount,ro
+
+}
+
+function manual_sync {
+    local action="$1" # either 'add' or 'delete'
+    local name="$2"
+    local sync="$3" # only needed for the 'add' action
+
+    local script_name="$HOME/bin/manual-sync-$name.sh"
+
+    case "$action" in
+        'add')
+            echo "$sync" > $script_name
+            chown $USER:$USER $script_name
+            chmod 775 $script_name
+
+            local symbol='#'
+            local header='manual sync script created at:'
+            generate_padding "$symbol" 4 "$header" "$script_name"
+            echo "$padding"
+            echo "$symbol $header $script_name $symbol"
+            echo "$padding"
+        ;;
+        'delete')
+            if [ -f "$script_name" ]; then
+                rm "$script_name"
+            fi
+
+            local symbol='#'
+            local header="manual sync script '$script_name' has been deleted"
+            generate_padding "$symbol" 4 "$header"
+            echo "$padding"
+            echo "$symbol $header $symbol"
+            echo "$padding"
+        ;;
+        *)
+            sudo mount / -o remount,ro
+            echo 'Internal error!' >&2
+            echo "in the '${FUNCNAME[0]}' function" >&2
+            exit 1
+        ;;
+    esac
+}
+
+function setup_sync {
+    local server_config_name="$1"
+    local name="$2"
+
+    local sync="export DISPLAY=:0.0 && export DBUS_SESSION_BUS_ADDRESS=\$(ps -u $USER e | grep -Eo 'dbus-daemon.*address=unix:abstract=/tmp/dbus-[A-Za-z0-9]{10}' | tail -c35) && /usr/bin/syncevolution $server_config_name"
+    local cron_entry="$CRON_FREQUENCY $sync"
+    local action='add'
+
+    cron_handler "$action" "$server_config_name" "$cron_entry"
 
     if [ ! -d $HOME/bin ]; then
         mkdir $HOME/bin
     fi
 
-    SCRIPT_NAME="$HOME/bin/manual-sync-$2.sh"
-    echo "$SYNC" > $SCRIPT_NAME
-    chmod +x $SCRIPT_NAME
-    echo "##############################################"
-    echo "# manual sync script created at $SCRIPT_NAME #"
-    echo "##############################################"
-    
-    sudo mount / -o remount,ro
+    manual_sync "$action" "$name" "$sync"
 }
 
 function delete {
-    SERVER_CONFIG_NAME="$1"
-    NAME="$2"
-    VISUAL_NAME="$3"
+    local server_config_name="$1"
+    local name="$2"
+    local visual_name="$3"
 
-    syncevolution --remove-database backend=evolution-contacts database="$VISUAL_NAME" &>/dev/null
-    syncevolution --remove "target-config@$SERVER_CONFIG_NAME"
-    syncevolution --remove "@$SERVER_CONFIG_NAME"
-    syncevolution --remove "$SERVER_CONFIG_NAME"
+    local action='delete'
+    cron_handler "$action" "$server_config_name"
+    manual_sync "$action" "$name"
+
+    syncevolution --remove-database backend=evolution-contacts \
+        database="${visual_name:0:30}" &>/dev/null
+    syncevolution --remove "target-config@${server_config_name:0:30}"
+    syncevolution --remove "@${server_config_name:0:30}"
+    syncevolution --remove "${server_config_name:0:30}"
     # I cannot find the `syncevolution` command to remove this configuration
-    rm --recursive --force $HOME/.config/syncevolution/default/sources/$2
+    rm --recursive --force \
+        $HOME/.config/syncevolution/default/sources/${name:0:30}
 }
 
 function delete-contacts {
-    delete "$CONTACTS_SERVER_CONFIG_NAME" "$CONTACTS_NAME" "$CONTACTS_VISUAL_NAME"
+    local i
+    for (( i = 0; i < ${#CONTACTS_NAMES[@]}; i++ )); do
+        delete "${CONTACTS_SERVER_CONFIG_NAMES[$i]}" \
+               "${CONTACTS_NAMES[$i]}" "${CONTACTS_VISUAL_NAMES[$i]}"
+    done
 }
 
 function delete-calendar {
-    delete "$CALENDAR_SERVER_CONFIG_NAME" "$CALENDAR_NAME" "$CALENDAR_VISUAL_NAME"
+    local i
+    for (( i = 0; i < ${#CALENDAR_NAMES[@]}; i++ )); do
+        delete "${CALENDAR_SERVER_CONFIG_NAMES[$i]}" \
+               "${CALENDAR_NAMES[$i]}" "${CALENDAR_VISUAL_NAMES[$i]}"
+    done
 }
 
 function contacts {
-    # add cron entry and create manual sync script
-    setup_sync "$CONTACTS_SERVER_CONFIG_NAME" "$CONTACTS_NAME"
+    local i
+    for (( i = 0; i < ${#CONTACTS_NAMES[@]}; i++ )); do
+        # syncevolution cannot handle names larger than 31 characters
+        local contacts_server_config_names="${CONTACTS_SERVER_CONFIG_NAMES[$i]:0:30}"
+        local contacts_names="${CONTACTS_NAMES[$i]:0:30}"
+        local contacts_visual_names="${CONTACTS_VISUAL_NAMES[$i]:0:30}"
 
-    #Create contact list
-    syncevolution --create-database backend=evolution-contacts database="$CONTACTS_VISUAL_NAME"
-    
-    #Create Peer
-    syncevolution --configure --template webdav username="$USERNAME" password="$PASSWORD" syncURL="$CONTACTS_URL" keyring=no "target-config@$CONTACTS_SERVER_CONFIG_NAME"
-    
-    #Create New Source
-    syncevolution --configure backend=evolution-contacts database="$CONTACTS_VISUAL_NAME" @default "$CONTACTS_NAME"
-    
-    #Add remote database
-    syncevolution --configure database="$CONTACTS_URL" backend=carddav "target-config@$CONTACTS_SERVER_CONFIG_NAME" "$CONTACTS_NAME"
-    
-    #Connect remote contact list with local databases
-    syncevolution --configure --template SyncEvolution_Client Sync=None syncURL="local://@$CONTACTS_SERVER_CONFIG_NAME" "$CONTACTS_SERVER_CONFIG_NAME" "$CONTACTS_NAME"
-    
-    #Add local database to the source
-    syncevolution --configure sync=two-way backend=evolution-contacts database="$CONTACTS_VISUAL_NAME" "$CONTACTS_SERVER_CONFIG_NAME" "$CONTACTS_NAME"
-    
-    #Start first sync
-    syncevolution --sync refresh-from-remote "$CONTACTS_SERVER_CONFIG_NAME" "$CONTACTS_NAME"
+        local url="$CONTACTS_URL${CARD_NAMES[$i]}"
+
+
+        # add cron entry and create manual sync script
+        setup_sync "${CONTACTS_SERVER_CONFIG_NAMES[$i]}" \
+                   "${CONTACTS_NAMES[$i]}"
+
+        #Create contact list
+        syncevolution --create-database backend=evolution-contacts \
+                                        database="$contacts_visual_names"
+
+        #Create Peer
+        #if (( ${#USERNAME} != 0 )) || (( ${#PASSWORD} != 0 )); then
+        if (( ${#PASSWORD} != 0 )); then
+            syncevolution --configure --template webdav username="$USERNAME" \
+                password="$PASSWORD" syncURL="$url" \
+                keyring=no "target-config@$contacts_server_config_names"
+        else
+            syncevolution --configure --template webdav syncURL="$url" \
+                keyring=no "target-config@$contacts_server_config_names"
+        fi
+
+        #Create New Source
+        syncevolution --configure backend=evolution-contacts \
+            database="$contacts_visual_names" @default "$contacts_names"
+
+        #Add remote database
+        syncevolution --configure database="$url" \
+            backend=carddav "target-config@$contacts_server_config_names" \
+            "$contacts_names"
+
+        #Connect remote contact list with local databases
+        syncevolution --configure --template SyncEvolution_Client \
+            Sync=None syncURL="local://@$contacts_server_config_names" \
+            "$contacts_server_config_names" "$contacts_names"
+
+        #Add local database to the source
+        syncevolution --configure sync=two-way backend=evolution-contacts \
+            database="$contacts_visual_names" "$contacts_server_config_names" \
+            "$contacts_names"
+
+        #Start first sync
+        syncevolution --sync refresh-from-remote \
+            "$contacts_server_config_names" "$contacts_names"
+    done
 }
-    
-function calendar {
-    # add cron entry and create manual sync script
-    setup_sync "$CALENDAR_SERVER_CONFIG_NAME" "$CALENDAR_NAME"
 
-    #Create Calendar
-    syncevolution --create-database backend=evolution-calendar database="$CALENDAR_VISUAL_NAME"
-    
-    #Create Peer
-    syncevolution --configure --template webdav username="$USERNAME" password="$PASSWORD" syncURL="$CAL_URL" keyring=no "target-config@$CALENDAR_SERVER_CONFIG_NAME"
-    
-    #Create New Source
-    syncevolution --configure backend=evolution-calendar database="$CALENDAR_VISUAL_NAME" @default "$CALENDAR_NAME"
-    
-    #Add remote database
-    syncevolution --configure database="$CAL_URL" backend=caldav "target-config@$CALENDAR_SERVER_CONFIG_NAME" "$CALENDAR_NAME"
-    
-    #Connect remote calendars with local databases
-    syncevolution --configure --template SyncEvolution_Client syncURL="local://@$CALENDAR_SERVER_CONFIG_NAME" "$CALENDAR_SERVER_CONFIG_NAME" "$CALENDAR_NAME"
-    
-    #Add local database to the source
-    syncevolution --configure sync=two-way database="$CALENDAR_VISUAL_NAME" "$CALENDAR_SERVER_CONFIG_NAME" "$CALENDAR_NAME"
-    
-    #Start first sync
-    syncevolution --sync refresh-from-remote "$CALENDAR_SERVER_CONFIG_NAME" "$CALENDAR_NAME"
+function calendar {
+    local i
+    for i in ${!CALENDAR_NAMES[@]}; do
+        # syncevolution cannot handle names larger than 31 characters
+        local calendar_server_config_names="${CALENDAR_SERVER_CONFIG_NAMES[$i]:0:30}"
+        local calendar_names="${CALENDAR_NAMES[$i]:0:30}"
+        local calendar_visual_names="${CALENDAR_VISUAL_NAMES[$i]:0:30}"
+
+        local url="$CAL_URL${CAL_NAMES[$i]}"
+
+        # add cron entry and create manual sync script
+        setup_sync "${CALENDAR_SERVER_CONFIG_NAMES[$i]}" "${CALENDAR_NAMES[$i]}"
+
+        #Create Calendar
+        syncevolution --create-database backend=evolution-calendar \
+                                        database="$calendar_visual_names"
+
+        #Create Peer
+        #if (( ${#USERNAME} != 0 )) || (( ${#PASSWORD} != 0 )); then
+        if (( ${#PASSWORD} != 0 )); then
+            syncevolution --configure --template webdav username="$USERNAME" \
+                password="$PASSWORD" syncURL="$url" keyring=no \
+                "target-config@$calendar_server_config_names"
+        else
+            syncevolution --configure --template webdav syncURL="$url" \
+                keyring=no "target-config@$calendar_server_config_names"
+        fi
+
+        #Create New Source
+        syncevolution --configure backend=evolution-calendar \
+            database="$calendar_visual_names" @default "$calendar_names"
+
+        #Add remote database
+        syncevolution --configure database="$url" backend=caldav \
+            "target-config@$calendar_server_config_names" "$calendar_names"
+
+        #Connect remote calendars with local databases
+        syncevolution --configure --template SyncEvolution_Client \
+            syncURL="local://@$calendar_server_config_names" \
+            "$calendar_server_config_names" "$calendar_names"
+
+        #Add local database to the source
+        syncevolution --configure sync=two-way \
+            database="$calendar_visual_names" "$calendar_server_config_names" \
+            "$calendar_names"
+
+        #Start first sync
+        syncevolution --sync refresh-from-remote \
+            "$calendar_server_config_names" "$calendar_names"
+    done
 }
 
 function help {
     echo 'Usage:'
-    echo "   $0 --contacts --calendar"
-    echo "   $0 --contacts"
-    echo "   $0 --calendar"
-    echo "   $0 --delete-contacts --delete-calendar"
-    echo "   $0 --delete-contacts"
-    echo "   $0 --delete-calendar"
+    echo "   $0 --contacts --calendar config1.txt [config2.txt ...]"
+    echo "   $0 --contacts config1.txt [config2.txt ...]"
+    echo "   $0 --calendar config1.txt [config2.txt ...]"
+    echo "   $0 --delete-contacts --delete-calendar config1.txt [config2.txt ...]"
+    echo "   $0 --delete-contacts config1.txt [config2.txt ...]"
+    echo "   $0 --delete-calendar config1.txt [config2.txt ...]"
     echo "   $0 -h | --help"
     exit 1
 }
@@ -140,7 +311,8 @@ if [ $# -eq 0 ]; then
     help
 fi
 
-TEMP=$(getopt --options 'h' --long 'calendar,contacts,delete-calendar,delete-contacts,help' --name "$0" -- "$@")
+TEMP=$(getopt --options 'h' --long 'calendar,contacts,delete-calendar,\
+       delete-contacts,help' --name "$0" -- "$@")
 
 if [ $? -ne 0 ]; then
     echo 'Terminating...' >&2
@@ -150,7 +322,10 @@ fi
 eval set -- "$TEMP"
 unset TEMP
 
-
+contacts=false
+calendar=false
+delete_calendar=false
+delete_contacts=false
 
 while true; do
     case "$1" in
@@ -158,33 +333,76 @@ while true; do
             help
         ;;
         '--contacts')
-            contacts
+            contacts=true
             shift
             continue
         ;;
         '--calendar')
-            calendar
+            calendar=true
             shift
             continue
         ;;
         '--delete-contacts')
-            delete-contacts
+            delete_contacts=true
             shift
             continue
         ;;
         '--delete-calendar')
-            delete-calendar
+            delete_calendar=true
             shift
             continue
         ;;
         '--')
-            # end of parameters
+            # end of flags
             shift
             break
         ;;
         *)
             echo 'Internal error!' >&2
+            echo 'in the flag decoding logic' >&2
             exit 1
         ;;
     esac
+done
+
+if $contacts && $delete_contacts; then
+    echo 'contradictory flags set: --contacts and --delete_contacts'
+    exit 1
+fi
+
+if $calendar && $delete_calendar; then
+    echo 'contradictory flags set: --calendar and --delete_calendar'
+    exit 1
+fi
+
+for config_file in "$@"; do
+    if [ -f "$config_file" ]; then
+        symbol='#'
+        header="using configuration file:"
+        generate_padding "$symbol" 4 "$header" "$config_file"
+        echo "$padding"
+        echo "$symbol $header $config_file $symbol"
+        echo "$padding"
+
+        source "$config_file"
+    else
+        echo "unable to read configuration file: $config_file, skipping"
+        continue
+    fi
+
+    if $calendar; then
+        calendar
+    fi
+
+    if $contacts; then
+        contacts
+    fi
+
+    if $delete_calendar; then
+        delete-calendar
+    fi
+
+    if $delete_contacts; then
+        delete-contacts
+    fi
 done
